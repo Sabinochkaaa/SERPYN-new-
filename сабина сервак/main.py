9577d556b12fa3979d85109af6e3898a57d635e2
Sabina Idrisova <sabinaidrisssova@gmail.com>
2026-08-01 23:45:11 +0500

    Update main.py

--- сабина сервак/main.py
+++ сабина сервак/main.py
@@ -26,7 +26,7 @@ from fastapi import FastAPI, HTTPException, Query, Request
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
-load_dotenv()
+#load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("serpyn")
